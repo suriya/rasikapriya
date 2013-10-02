6c773f834e14993c33f1b486786c2fce156ab6c9
Suriya Subramanian <suriya@alumni.cs.utexas.edu>
2013-10-02 11:43:12 +0530

    Installed django_autocomplete_light.

--- myproject/settings.py
+++ myproject/settings.py
@@ -136,11 +136,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'south',
+    'autocomplete_light',
     'rasikapriya',
 )
 
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
-# Uncomment the next two lines to enable the admin:
+import autocomplete_light
+autocomplete_light.autodiscover()
+
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
+    url(r'^autocomplete/', include('autocomplete_light.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
